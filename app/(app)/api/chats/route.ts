64319e446759adb63afe74d323159c1f58556d58
Chi Viet <vietchi296@gmail.com>
2025-01-21 23:14:22 +0700

    Hardcode api key

--- app/(app)/api/chats/route.ts
+++ app/(app)/api/chats/route.ts
@@ -5,8 +5,10 @@ import { PrivyClient } from '@privy-io/server-auth';
 import { findChatsByUser } from '@/db/services/chats';
 
 const privy = new PrivyClient(
-	process.env.NEXT_PUBLIC_PRIVY_APP_ID!,
-	process.env.PRIVY_APP_SECRET!
+	process.env.NEXT_PUBLIC_PRIVY_APP_ID ||
+		'fc-2152b9cb5d8f414c95c450277e74beb9',
+	process.env.PRIVY_APP_SECRET ||
+		'3ePht1EXY8BsNT6j5cJeRiMe3hJsGdmLVXgyV2XjjPcV2WjywfKro3jHJTCg2TDXtfzWs6zqAcixiXymREDtHJEY'
 );
 
 export const GET = async (req: NextRequest) => {
