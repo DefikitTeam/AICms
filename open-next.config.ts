e5f9ec52acc4a80a033fd40e6bfc2c278fdaa212
andt99 <andt14111999@gmail.com>
2025-04-03 11:18:05 +0700

    Update wrangler to 0.6.6

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,29 +1,6 @@
-import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
-import { default as cache } from "@opennextjs/cloudflare/kv-cache";
-import memoryQueue from "@opennextjs/cloudflare/memory-queue";
-
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
  
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      // set `incrementalCache` to "dummy" to disable KV cache
-      incrementalCache: async () => cache,
-      queue: async () => memoryQueue,
-      tagCache: "dummy",
-    },
-  },
- 
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
-
- 
-export default config;
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+});
