35f5d64689e865d23c98c889a9622a222e5810b1
binhtrongg <binhtrong0504@gmail.com>
2025-05-07 14:45:29 +0700

    remove unuse component,page

--- app/(app)/agents/market/_data/graph/edges.ts
+++
@@ -1,7 +0,0 @@
-import { Edge } from "@xyflow/react";
-
-export const edges: Edge[] = [
-    { id: 'e1-2', source: 'root', target: 'trending' },
-    { id: 'e1-3', source: 'root', target: 'token-data' },
-    { id: 'e1-4', source: 'root', target: 'token-data-2' },
-];
--- app/(app)/agents/market/_data/graph/index.ts
+++
@@ -1,9 +0,0 @@
-import { edges } from "./edges";
-import { nodes } from "./nodes";
-
-import { AgentGraph } from "../../../_types/agent";
-
-export const marketAgentGraph: AgentGraph = {
-    nodes,
-    edges,
-}
